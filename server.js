119bd94dc603e0f9683b2125c901b4b747f16ac0
dorkem <chlwogur1000@gmail.com>
2024-05-09 22:29:20 +0900

    페이지 나누는거 에러

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb')
 const { ObjectId } = require('mongodb') // id 찾을려면
+const methodOverride = require('method-override')
 require('dotenv').config();
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended:true})) 
+app.use(methodOverride('_method')) 
 
 let db
 const url = process.env.MONGO_URL;
@@ -84,3 +86,37 @@ app.get('/detail/:id', async (요청, 응답) => {
   }
 
 })
+
+app.get('/edit/:id', async (요청, 응답) => {
+  let result = await db.collection('post').findOne({ _id : new ObjectId(요청.params.id) })
+  응답.render('edit.ejs', {result : result})
+})
+
+app.put('/edit', async (요청, 응답)=>{
+  await db.collection('post').updateOne({ _id : new ObjectId(요청.body.id) },
+    {$set : { title : 요청.body.title, content : 요청.body.content }
+  })
+  응답.redirect('/list')
+}) 
+
+app.get('/abc/:id', async (요청, 응답) => {
+  console.log(요청.params)
+})
+
+app.delete('/delete', async (요청, 응답) => {
+  let result = await db.collection('post').deleteOne( { _id : new ObjectId(요청.query.docid) } )
+  응답.send('삭제완료')
+})
+
+
+app.get('/list/:id', async (요청, 응답) => {
+  let result = await db.collection('post').find()
+    .skip( (요청.params.id - 1) * 5 ).limit(5).toArray() 
+  응답.render('list.ejs', { 글목록 : result })
+
+}) 
+
+app.get('/list/next/:id', async (요청, 응답) => {
+  let result = await db.collection('post').find({_id : {$gt : new ObjectId(요청.params.id) }}).limit(5).toArray()
+  응답.render('list.ejs', { 글목록 : result })
+}) 
